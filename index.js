81373c281bfdb529a163b68d98f9dcfb824ffe84
Altemir Almeida <altemir.git@gmail.com>
2020-10-22 23:15:49 +0100

    Revert "fixe port"
    This reverts commit 54f048a143486f2e93b4354f7dd91319b7a95952.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-const hostname = '0.0.0.0';
+const port = 3000;
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(port, hostname, () => {
-  console.log(`App listening at http://${hostaname}:${port}`);
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
