1c666a8afa282bcf2f56ca866f3022826ccdb60d
Altemir Almeida <altemir.git@gmail.com>
2020-10-23 00:11:06 +0100

    Fixe Port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const hostname = '0.0.0.0';
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.listen(port, hostname, () => {
+  console.log(`App listening at http://${hostname}:${port}`);
 });
