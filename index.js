3c8f7478406b66a4f880216c63e45e26fbfa4648
Altemir Almeida <altemir.git@gmail.com>
2020-10-23 20:03:20 +0100

    Add project controller

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const users = require('./controllers/users')();
 const port = process.env.PORT || 3000;
 const hostname = '0.0.0.0';
 const app = (module.exports = express());
+
+const users = require('./controllers/users')();
+const projects = require('./controllers/projects')();
 app.use(bodyParser.json());
 
 app.get('/users', users.getController);
 app.get('/users/:email', users.getByEmail);
 app.post('/users', users.postController);
+
+app.get('/projects', projects.getController);
+app.get('/projects/:slug', projects.getBySlug);
+app.post('/projects', projects.postController);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
