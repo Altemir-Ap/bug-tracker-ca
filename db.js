1691ed82cdc45266d7393c1e2bdbce72e7b31753
Altemir Almeida <altemir.git@gmail.com>
2020-10-23 18:57:34 +0100

    Add db connection

---
+++ db.js
@@ -0,0 +1,35 @@
+const uri = process.env.MONGO_URI;
+const MongoClient = require('mongodb').MongoClient;
+const MONGO_OPTIONS = { useUnifiedTopology: true, useNewUrlParser: true };
+const DB_NAME = 'cbwa';
+module.exports = () => {
+  const get = (collectionName, query = {}) => {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(uri, MONGO_OPTIONS, (err, client) => {
+        const db = client.db(DB_NAME);
+        const collection = db.collection(collectionName);
+        collection.find(query).toArray((err, docs) => {
+          resolve(docs);
+          client.close();
+        });
+      });
+    });
+  };
+
+  const add = (collectionName, item) => {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(uri, MONGO_OPTIONS, (err, client) => {
+        const db = client.db(DB_NAME);
+        const collection = db.collection(collectionName);
+        collection.insertOne(item, (err, result) => {
+          resolve(result);
+          client.close();
+        });
+      });
+    });
+  };
+  return {
+    get,
+    add,
+  };
+};
